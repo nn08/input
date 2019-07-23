2fb8fa3ab1d72cb541afe6a669066def29086869
nn08 <53204486+nn08@users.noreply.github.com>
2019-07-23 18:42:05 +0800

    Delete description.ts

--- src/src/app/description.ts
+++
@@ -1,4 +0,0 @@
-export class Description {
-   constructor(public skill_id: string, public category: string, public family: string, public set: string, public skill_item: string, public rate: number) {
-   }
-}
