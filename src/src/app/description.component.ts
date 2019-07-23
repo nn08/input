b1fa0e8c4b3739483803eb2c8747cc7c918f9b31
nn08 <53204486+nn08@users.noreply.github.com>
2019-07-23 18:37:44 +0800

    Add files via upload

---
+++ src/src/app/description.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { DescriptionService } from './description.service';
+import { Description } from './description';
+import { DescriptionComponent } from './description.component';
+
+@Component({
+   selector: 'app-description',
+   templateUrl: './description.component.html',
+   styleUrls: ['./description.component.css']
+})
+export class DescriptionComponent implements OnInit {
+   //Component properties
+   allDescriptions: Description[];
+   statusCode: number;
+   requestProcessing = false;
+   recordIdToUpdate = null;
+   resultToEdit = false;
+   processValidation = false;
+   //Create form
+   descriptionForm = new FormGroup({
+     rate: new FormControl('', Validators.required)
+   });
+
+   //Create constructor to get service instance
+   constructor(private descriptionService: DescriptionService) {
+   }
+   //Create ngOnInit() and load records
+   ngOnInit(): void {
+	   this.getAllDescriptions();
+   }
+   //Fetch all records
+   getAllDescriptions() {
+		this.descriptionService.getAllDescriptions()
+		  .subscribe(
+                data => this.allDescriptions = data;
+                this.displayData= true;
+				errorCode =>  this.statusCode = errorCode);
+
+   }
+
+   //create new records
+   createNewRecord(form: descriptionForm) {
+     console.log(form.value)
+      this.preProcessConfigurations();
+      let arr[];
+      for(let i=0; i< this.descriptionForm.length; i++){
+        arr.push(this.BuildFormDynamic(this.descriptionForm[i]))
+      }
+      //this.rateForm.setValue({ skill_id: descriptionForm.skill_id });
+      //let rate = this.descriptionForm.value.rate;
+      this.descriptionService.createRecord(arr.rate)
+      .subscribe(description => {
+        console.log(description);
+        this.processValidation = true;
+        this.requestProcessing = false;
+		   },
+		   errorCode =>  this.statusCode = errorCode);
+   }
+/*   //Load record by id to edit
+   loadRecordToEdit(skillId: string) {
+      this.preProcessConfigurations();
+      this.recordService.getRecordById(skillId)
+	      .subscribe(record => {
+			console.log(record,'poiuytre');
+		            this.recordIdToUpdate = record.id;
+					this.recordForm.setValue({ period: record.period, rating: record.rating });
+					this.processValidation = true;
+					this.requestProcessing = false;
+		        },
+		        errorCode =>  this.statusCode = errorCode);
+   }
+
+   //CNN Load record by priod to edit
+   loadRecordByPeriod(recordPeriod: string) {
+      this.preProcessConfigurations();
+      this.resultToEdit = true;
+      this.recordService.getRecordByPeriod(recordPeriod).subscribe (
+          data => this.allRecords = data,
+          errorCode =>  this.statusCode = errorCode
+   )
+ }
+
+ //Handle create and update record
+ onRecordFormSubmit() {
+  this.processValidation = true;
+  if (this.recordForm.invalid) {
+       return; //Validation failed, exit from method.
+  }
+  //Form is valid, now perform create or update
+    this.preProcessConfigurations();
+    let period = this.recordForm.value.period;
+      this.loadRecordByPeriod(period);
+ }
+
+   //Delete record
+   deleteRecord(recordId: string) {
+      this.preProcessConfigurations();
+      this.recordService.deleteRecordById(recordId)
+	      .subscribe(successCode => {
+		            //this.statusCode = successCode;
+					//Expecting success code 204 from server
+					this.statusCode = 204;
+				    this.getAllRecords();
+				    this.backToCreateRecord();
+			    },
+		        errorCode => this.statusCode = errorCode);
+   }
+   */
+   //Perform preliminary processing configurations
+   preProcessConfigurations() {
+      this.statusCode = null;
+	  this.requestProcessing = true;
+   }
+   //Go back from update to create
+  /* backToCreateRecord() {
+      this.recordIdToUpdate = null;
+      this.descriptionForm.reset();
+	  this.processValidation = false;
+  }*/
+}
---
+++ src/src/app/description.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { Description } from './description';
+import { Rate } from './description';
+
+
+@Injectable()
+export class DescriptionService {
+    //URL for CRUD operations
+	descriptionUrl = "http://localhost:3000/skills_description";
+	//Create constructor to get Http instance
+	constructor(private http:Http) {
+	}
+
+	//Fetch all descriptions
+    getAllDescriptions(): Observable<Description[]> {
+        return this.http.get(this.descriptionUrl+"/get-all-items")
+		   		.map(this.extractData)
+		        .catch(this.handleError);
+
+    }
+	//Create record
+    createRecord(rate: Rate):Observable<Rate[]> {
+	    let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: cpHeaders });
+        return this.http.post(this.descriptionUrl+"/create-record", rate, options)
+               .map(success => success.status)
+               .catch(this.handleError);
+    }
+	//Fetch record by id
+  /*  getRecordById(recordId: string): Observable<Record> {
+		let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: cpHeaders });
+		console.log(this.descriptionUrl +"/get-record-by-id?id="+ recordId);
+		return this.http.get(this.descriptionUrl +"/get-record-by-id?id="+ recordId)
+			   .map(this.extractData)
+			   .catch(this.handleError);
+    }
+
+		//Fetch record by period - CNN
+		getRecordByPeriod(recordPeriod: string): Observable<Record[]> {
+				//let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
+			  //let options = new RequestOptions({ headers: cpHeaders });
+		//	console.log(this.descriptionUrl +"/get-record-by-period?period="+ recordPeriod);
+			return this.http.get(this.descriptionUrl +"/get-record-by-period?period="+ recordPeriod)
+					 .map(this.extractData)
+					 .catch(this.handleError);
+			}
+
+	//Update record
+    updateRecord(record: Record):Observable<number> {
+	    let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
+			let options = new RequestOptions({ headers: cpHeaders });
+        return this.http.put(this.descriptionUrl +"/update-record", record, options)
+               .map(success => success.status)
+               .catch(this.handleError);
+    }
+    //Delete record
+    deleteRecordById(recordId: string): Observable<number> {
+		let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: cpHeaders });
+		return this.http.delete(this.descriptionUrl +"/delete-record?id="+ recordId)
+			   .map(success => success.status)
+			   .catch(this.handleError);
+    }*/
+		private extractData(res: Response) {
+			let body = res.json();
+			return body;
+    }
+    private handleError (error: Response | any) {
+			console.error(error.message || error);
+			return Observable.throw(error.status);
+    }
+}
